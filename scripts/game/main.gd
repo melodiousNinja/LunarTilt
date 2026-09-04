82632b9315ba089218bd6a4650a254ebf725c508
melodiousNinja <lalitsinghal93@gmail.com>
2026-09-04 13:57:53 +0530

    banner: factory-v5-20260904 (v5 code shipped with stale v4 stamp - device banner must be trustworthy)

--- scripts/game/main.gd
+++ scripts/game/main.gd
@@ -69,7 +69,7 @@ func _ready() -> void:
 		world.spawn_hand_ball("red")
 		world.spawn_hand_ball("black")
 	_give_active_ball()
-	print("SCB_MAIN_VERSION=official-v4-20260904 SLING_K=%s serve=%s" % [
+	print("SCB_MAIN_VERSION=factory-v5-20260904 SLING_K=%s serve=%s" % [
 		SLING_K, (active_ball.position if active_ball != null else Vector3.INF)])
 
 
